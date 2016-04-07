6879f7eec35f756c52f6ba0145524c62e281387a
Angamanga <ki222bj@student.lnu.se>
2016-04-07 15:21:03 +0200

    code from http://thejackalofjavascript.com/rpi-live-streaming/

--- app.js
+++ app.js
@@ -1,5 +1,79 @@
 "use strict";
 const dotenv = require('dotenv').config(),
-    controller = require('./server/controller');
+    controller = require('./server/controller'),
+    express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http),
+    fs = require('fs'),
+    path = require('path'),
+    spawn = require('child_process').spawn;
 
+var proc;
+console.log(spawn);
+console.log(proc);
 controller.init();
+
+app.use('/', express.static(path.join(__dirname, 'stream')));
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+var sockets = {};
+
+io.on('connection', function(socket) {
+
+    sockets[socket.id] = socket;
+    console.log("Total clients connected : ", Object.keys(sockets).length);
+
+    socket.on('disconnect', function() {
+        delete sockets[socket.id];
+
+        // no more sockets, kill the stream
+        if (Object.keys(sockets).length == 0) {
+            app.set('watchingFile', false);
+            if (proc) proc.kill();
+            fs.unwatchFile('./stream/image_stream.jpg');
+        }
+    });
+
+    socket.on('start-stream', function() {
+        startStreaming(io);
+    });
+
+});
+
+http.listen(3000, function() {
+    console.log('listening on *:3000');
+});
+
+function stopStreaming() {
+    if (Object.keys(sockets).length == 0) {
+        app.set('watchingFile', false);
+        if (proc) proc.kill();
+        fs.unwatchFile('./stream/image_stream.jpg');
+    }
+}
+
+function startStreaming(io) {
+    console.log(io);
+
+    if (app.get('watchingFile')) {
+        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+        return;
+    }
+
+    var args = ["-w", "640", "-h", "480", "-o", "./stream/image_stream.jpg", "-t", "999999999", "-tl", "100"];
+    proc = spawn('raspistill', args);
+
+    console.log('Watching for changes...');
+
+    app.set('watchingFile', true);
+
+    fs.watchFile('./stream/image_stream.jpg', function(current, previous) {
+        io.sockets.emit('liveStream', 'image_stream.jpg?_t=' + (Math.random() * 100000));
+    });
+
+}
