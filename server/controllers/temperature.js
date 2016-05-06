409ea82ff5b15a79a2bf92f4e5d56681ba36e5b5
Anna Iosif <Angamanga@Annas-MacBook-Pro.local>
2016-05-07 00:08:24 +0200

    error handeling temp

--- server/controllers/temperature.js
+++ server/controllers/temperature.js
@@ -4,12 +4,11 @@ const ds18b20 = require('ds18b20');
 module.exports = {
     measureTemperature()
     {return new Promise((resolve, reject)=> {
-        ds18b20.sensors(function (err, ids) {
-            console.log(err);
-            console.log(ids);
-        });
-
         ds18b20.temperature('28-0115907b5dff', function (err, value) {
+            if(err){
+                console.log(err);
+                resolve('no temp measured');
+            }
             console.log('Current temperature is', value);
             resolve(value);
         });
